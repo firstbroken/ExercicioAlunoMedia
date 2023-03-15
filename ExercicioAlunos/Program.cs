ac162dc8165c28f32193bd022f3a4f2bcecd601d
Renan Ferreira <100379663+firstbroken@users.noreply.github.com>
2023-03-15 16:22:01 -0300

    Add files via upload

--- ExercicioAlunos/Program.cs
+++ ExercicioAlunos/Program.cs
@@ -3,11 +3,22 @@ class Program
 {
     static void Main(string[] args)
     {
+
         Alunos aluno1 = new Alunos();
-        aluno1.nome = "Fulano da Silva";
+        aluno1.nome = "Outro aluno";
         aluno1.nota1 = 4;
         aluno1.nota2 = 5;
-
         aluno1.mensagem();
+
+        Console.Write("Digite seu nome: ");
+        string? nome = Console.ReadLine();
+        Console.WriteLine("Seu nome é: " + nome);
+        Console.Write("Digite a primeira nota: ");
+        double nota1 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("O número digitado foi: " + nota1);
+        Console.Write("Digite a segunda nota: ");
+        double nota2 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("O número digitado foi: " + nota2);
+        Console.WriteLine("O aluno " + nome + "está com a média de:" + (nota1+nota2)/2);
     }
 }
